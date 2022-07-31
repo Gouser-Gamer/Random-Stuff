5904dd5616baa2475d1622b3f38d63ae005e1ff9
Gouser-Gamer <83184412+Gouser-Gamer@users.noreply.github.com>
2022-07-31 06:34:41 -0700

    Add files via upload

---
+++ --List of games.lua
@@ -0,0 +1,22 @@
+--List of games
+
+if game.PlaceId == 2916899287 then
+    _G.Ambient = false;
+end
+
+if game.PlaceId == 2569453732 then
+    _G.Ambient = false;
+end
+
+if game.PlaceId == 292439477 then
+    _G.Ambient = false;
+end
+
+if game.PlaceId == 4588604953 then
+    _G.Ambient = false;
+end
+
+if game.PlaceId == 2534724415 then
+    _G.Ambient = false;
+end
+
